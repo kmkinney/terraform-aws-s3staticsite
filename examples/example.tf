0db05df79d8370cf04cd5dd5dae2cf45e4e79a7e
Jamie Visker <jamievisker@gmail.com>
2020-05-08 15:29:16 -0600

    improve example

--- examples/example.tf
+++ examples/example.tf
@@ -14,8 +14,8 @@ data "aws_route53_zone" "zone" {
 }
 
 module "s3_site" {
-  //  source    = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v2.0.0"
-  source         = "../."
+  source = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v2.0.0"
+  //source         = "../."
   site_url       = "teststatic.byu-oit-terraform-dev.amazon.byu.edu"
   hosted_zone_id = data.aws_route53_zone.zone.id
   s3_bucket_name = "terraform-module-dev-s3staticsite"
