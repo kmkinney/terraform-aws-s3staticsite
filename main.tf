c9483cb679e3a9b55f130897afef5ccad3e0dab7
Blake Morgan <blake_morgan@byu.edu>
2020-08-10 15:59:22 -0600

    Reformat tf

--- main.tf
+++ main.tf
@@ -25,11 +25,11 @@ resource "aws_acm_certificate_validation" "cert" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-  for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  }
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
 
   provider = aws.aws_n_va
