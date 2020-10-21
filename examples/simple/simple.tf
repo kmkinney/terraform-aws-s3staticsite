2ded6a62bf3b7113d95e4282a57d7bebbe901188
Blake Morgan <blake_morgan@byu.edu>
2020-10-21 09:12:06 -0600

    Version number

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -10,7 +10,7 @@ data "aws_route53_zone" "zone" {
 }
 
 module "s3_site" {
-  source = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v5.1.0"
+  source = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v5.1.1"
   //  source         = "../../"
   site_url       = "teststatic.byu-oit-terraform-dev.amazon.byu.edu"
   hosted_zone_id = data.aws_route53_zone.zone.id
